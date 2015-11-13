12e72aee5e75ccd2aeb682d9d7363ab8ecf34bd0
Serafeim Papastefanos <spapas@gmail.com>
2015-11-13 14:59:17 +0200

    Ok

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import {connect} from 'react-redux';
 import Winner from './Winner';
+import * as actionCreators from '../action_creators';
 
-export default React.createClass({
+export const Results = React.createClass({
   mixins: [PureRenderMixin],
   getPair: function() {
     return this.props.pair || [];
@@ -33,7 +35,20 @@ export default React.createClass({
                    onClick={this.props.next}>
             Next
           </button>
-        </div>
+      </div>
       </div>;
   }
 });
+
+function mapStateToProps(state) {
+  return {
+    pair: state.getIn(['vote', 'pair']),
+    tally: state.getIn(['vote', 'tally']),
+    winner: state.get('winner')
+  }
+}
+
+export const ResultsContainer = connect(
+  mapStateToProps,
+  actionCreators
+)(Results);
